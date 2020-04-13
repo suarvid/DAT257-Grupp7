c6b5447504cace1e3cfb339b2f9a61fcdc1f5326
Arvid Sundbom <suarvid@student.chalmers.se>
2020-04-13 21:09:50 +0200

    Initial commit before django branching

---
+++ src/ArresComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ArresComponent() {
+    return (
+        <h1>HTML är ju rätt trökigt ändå</h1>
+        <p>Med en massa taggar och ståhej</p>
+    )
+}
+
+export default ArresComponent
