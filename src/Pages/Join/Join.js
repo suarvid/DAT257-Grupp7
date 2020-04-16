2eddadf66c720bbaa78fd9641fd99ff21118fd66
isaaklindgren <isak_lindgren_5@hotmail.com>
2020-04-16 13:47:09 +0200

    first,very basic component

---
+++ src/Pages/Join/Join.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Join.css'
+import IsaksComponent from '../../IsaksComponent'
+
+function Join() {
+    return (
+        <div className="join-container">
+            <IsaksComponent />
+        </div>
+    )
+}
+
+export default Join
