85b566d3967d44248d0cb472e7b00c7b18111eb9
Arvid Sundbom <suarvid@student.chalmers.se>
2020-04-16 15:45:19 +0200

    added redirect upon deleting a post

--- src/Pages/Home/PostDetailView.js
+++ src/Pages/Home/PostDetailView.js
@@ -1,34 +1,43 @@
 import React from "react";
 import axios from "axios";
 import { Card , Button } from 'antd';
-import { Redirect, useHistory } from "react-router-dom";
-
+import { Redirect } from 'react-router-dom'; 
 class PostDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            post: {},
-            postID: this.props.match.params.postID
+            post: {
+                ID: this.props.match.params.postID
+            },
+            redirect: false
         }
     } 
 
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to='/' />
+        }
+    }
+
  //Note: important to be consistent with trailing slashes in frontend and backend
     handleDelete = (event) => {
-        axios.delete(`http://localhost:8000/api/post/${this.state.postID}/`, {
+        this.setState({
+            redirect: true
+        })
+        const postID = this.props.match.params.postID;
+        axios.delete(`http://localhost:8000/api/post/${postID}/`, {
             data: {
-                id: this.state.postID,
+                id: this.state.post.ID,
             },
         })
         .then( response =>  {
             console.log(response);
             console.log(response.data);
         });
-        let history = useHistory();
-        history.pushState("/");
     }
     
     componentDidMount() {
-        axios.get(`http://localhost:8000/api/post/${this.state.postID}`)
+        axios.get(`http://localhost:8000/api/post/${this.state.post.ID}`)
             .then( response => {
                 this.setState({
                     post: response.data
@@ -38,12 +47,15 @@ class PostDetail extends React.Component {
     
     render() {
         return (
+            <div>
+                {this.renderRedirect()}
                 <Card title={this.state.post.title}>
                     <p> {this.state.post.content} </p>
                         <form>
-                            <Button type="danger" htmlType="submit" onClick={this.handleDelete}>Delete Post</Button>
+                                <Button type="danger" htmlType="submit" onClick={this.handleDelete}>Delete Post</Button>
                         </form>
                 </Card>
+            </div>
         );
     }
 }
