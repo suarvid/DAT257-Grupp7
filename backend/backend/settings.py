859d7d2d03e8aec3cb0c48079223ec8c092221e3
Arvid Sundbom <suarvid@student.chalmers.se>
2020-04-17 09:16:03 +0200

    changed allowed hosts to include localhost

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=3_$haupanvj5$x@$b%in1&7m9q$xuh(=crapcdxzjb*m362n4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sjuanbackend.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
