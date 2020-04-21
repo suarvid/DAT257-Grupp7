3e2eebb8ea702ae936e86894fff08647d357388c
Arvid Sundbom <suarvid@student.chalmers.se>
2020-04-21 18:24:53 +0200

    Added initial structure for infinite scroller on home page

--- src/Components/NewsItem/NewsItem.js
+++ src/Components/NewsItem/NewsItem.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import './newsitem.css'
-<<<<<<< HEAD
-import { Link, Route, withRouter, BrowserRouter as Router, Switch, NavLink } from 'react-router-dom';
-=======
-import { Typography, Slider, Switch } from 'antd';
-import { withRouter, Redirect, Link, NavLink, Route, BrowserRouter as Router } from 'react-router-dom';
-import Login from '../../Pages/Login/Login';
->>>>>>> c76ea777a8f70f5381bf87de9a35bb5904f63d48
+
+import { withRouter, Link, Route } from 'react-router-dom';
 import PostDetailView from '../../Pages/Home/PostDetailView';
-import { Layout } from 'antd';
 
 class NewsItem extends React.Component {
     constructor(props) {
