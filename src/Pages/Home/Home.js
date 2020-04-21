a3a01019e533c0715c779ec99a6f7e928fd1cef4
dahlqvist96 <dahlqvistsanna@gmail.com>
2020-04-21 11:35:53 +0200

    Fixat sidorna: Om oss, vill du synas här och startsidan ink. banner

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import BaseRouter from '../../routes.js'
-
+import './Home.css'
+import BannerComponent from '../../BannerComponent'
 function Home() {
     return (
         <div>
+        
+            <div className="first">
+                <div className="banner">
+                    <h3>Ingående företag</h3>
+                    <BannerComponent/>       
+                </div>
+            </div>
+            <div className="second">
+                <div className="aktiviteter">
+                    <h3>Aktiviteter i närtid</h3>
+                </div>
+            </div>
+            
             <BaseRouter />
         </div>
     )
