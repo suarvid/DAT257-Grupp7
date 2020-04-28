0e7f36d90f0472a057e3f49e23c0081753879d1f
dahlqvist96 <dahlqvistsanna@gmail.com>
2020-04-28 09:44:16 +0200

    fixat footern på home-sidan

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,18 +9,20 @@ import './Home.css'
 function Home() {
     return (
             <div>
-                <div>
-                    <div className="first">
-                        <div className="banner">
-                            <h3>Ingående företag</h3>
-                            <BannerComponent/>
-                            <BaseRouter />  
+                <div className="home-container">
+                    <div>
+                        <div className="first">
+                            <div className="banner">
+                                <h3>Ingående företag</h3>
+                                <BannerComponent/>
+                                <BaseRouter />  
+                            </div>
                         </div>
-                    </div>
-                
-                    <div className="second">
-                        <div className="aktiviteter">
-                            <h3>Aktiviteter i närtid</h3>
+                    
+                        <div className="second">
+                            <div className="aktiviteter">
+                                <h3>Aktiviteter i närtid</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
