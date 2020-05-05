e395c2510ce8b15be28f4d52d9e833060278c19d
ErikaNystrom <nyerika@student.chalmers.se>
2020-05-05 16:34:31 +0200

    mobilanpassat Join-sidan

--- src/Pages/Join/Join.js
+++ src/Pages/Join/Join.js
@@ -3,23 +3,23 @@ import './Join.css'
 
 function Join() {
     return (
-        <div>
             <div className="join-container">
-                <h1>Vill du synas här?</h1>
                 <div className="info-text">
-                <h2>Vi på Hela Åsa letar ständigt efter nya partners i vårt hälso-kollektiv. Är du intresserad av att bli del i Hela Åsa?</h2> 
+                <h2>Vill du synas här?</h2>
+                <h3>Vi på Hela Åsa letar ständigt efter nya partners i vårt hälso-kollektiv. Är du intresserad av att bli del i Hela Åsa?</h3> 
                 <p> Kontakta oss gärna via mail så kan vi diskutera vårt framtida partnerskap!</p>
+                <p> Med Vänliga Hälsningar <br></br>
+                    Hela Åsa <br></br>
+                    info@helaasa.se </p>
                 </div>
                 
                 <div className="companies">
                     <h3>Ingående företag</h3>
-                    <p>Åsa Yoga</p>
-                    <p>Yoga Mamma</p>
-                    <p>Helens Dans</p>
+                    <a href="https://www.facebook.com/yogamidus"> Yogamidus</a>
+                    <a href="https://www.facebook.com/niamotion/">Niamotion</a>
+                    <a href="https://www.facebook.com/unikarum/">UnikaRum</a>
                 </div>
             </div>
-            
-        </div>
     )
 }
 
