2e2e0887eb8f9b9399aa1f44ebbaf9f5f014fda0
Arvid Sundbom <suarvid@student.chalmers.se>
2020-05-06 10:44:34 +0200

    Fixed activities in instructor details, also images look slightly better now

--- backend/instructors/models.py
+++ backend/instructors/models.py
@@ -11,6 +11,14 @@ class Instructor(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, **kwargs):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 class InstructorActivities(models.Model):
     instructorID = models.ForeignKey(Instructor, on_delete = models.CASCADE)
     activityID = models.ForeignKey(Activity, on_delete = models.CASCADE)
