b4179078489727b1294f1f2647c894bcc627ecf4
matteB10 <blomquistmatilda@hotmail.com>
2020-05-06 11:14:11 +0200

    fixed mobile adapted css for activities, changed back to getting data from backend

--- src/Pages/Activities/Activity/Activity.js
+++ src/Pages/Activities/Activity/Activity.js
@@ -4,12 +4,14 @@ import './Activity.css'
 export default function Activity({ activityType, imageUrl, description }) {
     // Can use activityType in future for nesting linking to details pages 
     console.log("Activity type: " + activityType)
-    console.log(imageUrl);
+    console.log("imageURl in activiry " + imageUrl);
     return (
         <div className="activityCard">
-            <img className="activityImage" src={imageUrl} alt={activityType}></img>
-            <div className="descriptionPreview">
+            <div id = "activityHeader">
+                <img className="activityImage" src={imageUrl} alt={activityType}></img>
                 <h2 style = {{textDecoration: "none"}}>{activityType}</h2>
+            </div>
+            <div className="descriptionPreview">
                  <p>{description}</p>
             </div>
             
