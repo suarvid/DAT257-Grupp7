396b2b70f88adc0095d745998d770e08b007ab56
Arvid Sundbom <suarvid@student.chalmers.se>
2020-05-07 15:45:44 +0200

    added logic for preventing locations getting double-booked

--- backend/classes/models.py
+++ backend/classes/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from instructors.models import Instructor
 from activity.models import Activity
 from locations.models import Location
+from django.core.exceptions import ValidationError
 
 
 class Class(models.Model):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     description = models.CharField(max_length=240, blank=True)
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    instructor = models.ForeignKey(
+        Instructor, on_delete=models.CASCADE, default=0)
     date = models.DateField(default=timezone.localdate)
     start_time = models.TimeField(default=timezone.localtime)
     end_time = models.TimeField(default=timezone.localtime)
@@ -19,9 +21,17 @@ class Class(models.Model):
 
     def save(self, *args, **kwargs):
         if (self.start_time > self.end_time) or (self.registered_attendees > self.max_attendees and self.max_attendees != 0):
-            return
+            raise ValidationError(
+                'The start time cannot be set to after the end time!')
+        elif self.registered_attendees > self.max_attendees and self.max_attendees != 0:
+            raise ValidationError(
+                'The class is full and cannot register more attendees!')
         elif self.max_attendees > self.location.capacity:
-            return
+            raise ValidationError(
+                'The max attendees of the class cannot be set to higher than the capacity of the class location!')
+        elif Class.objects.filter(start_time__lte=self.start_time, end_time__gt=self.start_time, date=self.date, location=self.location).exists() or Class.objects.filter(start_time__gte=self.start_time, start_time__lt=self.end_time, date=self.date, location=self.location).exists():
+            raise ValidationError(
+                'The set location is already booked during the given time.')
         else:
             super().save(*args, **kwargs)
 
