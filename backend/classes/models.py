f0f892bfe3b941d294e9da05afa913a3e2bad3b4
Arvid Sundbom <suarvid@student.chalmers.se>
2020-05-07 14:43:13 +0200

    added locations as a foreign key in model for classes

--- backend/classes/models.py
+++ backend/classes/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from activity.models import Activity
+from locations.models import Location
 
 
 class Class(models.Model):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     description = models.CharField(max_length=240, blank=True)
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
     date = models.DateField(default=timezone.localdate)
     start_time = models.TimeField(default=timezone.localtime)
     end_time = models.TimeField(default=timezone.localtime)
-    location = models.CharField(max_length=120)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=0)
     max_attendees = models.IntegerField(default=0)
     registered_attendees = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
@@ -21,7 +22,6 @@ class Class(models.Model):
             return
         else:
             super().save(*args, **kwargs)
-
 
     def __str__(self):
         return self.activity.name
