95b691a9995a72a8c2e3bad40244b0df25eb71fe
Joel Rudsberg <rudsberg@live.se>
2020-05-07 12:17:43 +0200

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import "./index.css"
-import Navbar from './Navigation/NavigationBar/NavigationBar';
-import HamburgerMenu from './Navigation/NavigationBar/HamburgerMenu';
-import BaseRouter from './routes';
-import Footer from './Components/FooterComponent/FooterComponent';
-import Banner from './Components/Banner/BannerComponent';
-
+import "./index.css";
+import HamburgerMenu from "./Navigation/NavigationBar/HamburgerMenu";
+import BaseRouter from "./routes";
+import Footer from "./Components/FooterComponent/FooterComponent";
+import Banner from "./Components/Banner/BannerComponent";
 
 export default function App() {
   return (
     <div>
-    <Banner/>
-    <HamburgerMenu />
-    <BaseRouter></BaseRouter>
-    <Footer/>
+      <Banner />
+      <HamburgerMenu />
+      <BaseRouter></BaseRouter>
+      <Footer />
     </div>
   );
 }
