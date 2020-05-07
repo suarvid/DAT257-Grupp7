38c39ea2770d0bcb0af03703562ff2e19853df39
isaaklindgren <isak_lindgren_5@hotmail.com>
2020-05-07 11:42:40 +0200

    added props and stuff

--- src/Pages/Prices/Prices.js
+++ src/Pages/Prices/Prices.js
@@ -1,36 +1,48 @@
 import React from "react";
 import "./Prices.css";
+import PriceComponent from "./PriceComponent";
+
+//Detta ska då simulera data som kommer från en en databas eller liknande, men nu hårdkodar jag detta utan att veta varken vilken data som finns eller om vi vi kommer ha
+//denna data i databasen eller inte.
+// Så detta är förmodligen något en skulle behöva göra om om vi stoppade in det i databasen på riktigt, men det är en  start.
+//Bytte även ut x och y mot "riktiga priser" bara för att det ska gå se senare, detta går såklart också lägga variabler för, men vet inte hur detta kommer se ut i framtiden.
+const data = [
+  {
+    title: "Yoga",
+    description: "En timmes pass av Ying yoga eller Yang yoga.",
+    price: "Ett pass X 99:-",
+    multicard: "Klippkort 10 ggr yoga 990:-",
+  },
+  {
+    title: "Dans",
+    description: "En timmes dans av typen Zumba. ",
+    price: "Ett pass X 78:-",
+    multicard: "Klippkort 10 ggr dans 700:-",
+  },
+];
 
 function Prices() {
-    return (
-            <div className="price-container">
-                <h2> Hela Åsas priser Sommaren 2020</h2>
-                <div className="activity">
-                  <h4>Yoga</h4>
-                  <p>En timmes pass av Ying yoga eller Yang yoga. </p>
-                  <p>Ett pass X :-</p>
-                  <p>Klippkort 10 ggr yoga Y:-</p>
-
-                </div>
-                <div className="activity">
-                  <h4>Dans</h4>
-                  <p>En timmes dans av typen Zumba. </p>
-                  <p>Ett pass X :-</p>
-                  <p>Klippkort 10 ggr yoga Y:-</p>
-                </div>
-
-              
-
-            </div>
-    ) 
+  return (
+    <div className="price-container">
+      {/* här skapar jag komponenter utifrån datan i stället för att skriva datan direkt i koden, det gör det enklare för mig om jag skulle lägga till flera typer
+              av pass och priskategorier senare. Så som sagt tidigare så antar jag vilka datafält som kan vara intressanta.*/}
+      <h2> Hela Åsas priser Sommaren 2020</h2>
+      {/* Förstår om ni inte förstår detta, men det map-funktionen gör är typ samma sak som en for-loop, och det "item" som står där är den informationen som finns
+      på varje index i data-listan som jag definierade ovan. Så tänk detta som en for-loop som tar varje index och skickar informationen till pricecomponent. */}
+      {data.map((item) => (
+        <PriceComponent data={item} />
+      ))}
+    </div>
+  );
 }
 
 /*
 getPrice() {
     return this.state.price;
   }
-*/ 
-export default Prices
+*/
+
+export default Prices;
 /*Class Prices extends react.component{}
   constructor(props){
         super(props);
