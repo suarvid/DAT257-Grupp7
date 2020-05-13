d62d5f42e5c98499ab8e8500ffaf0d79ecdfe163
Arvid Sundbom <suarvid@student.chalmers.se>
2020-05-13 16:05:35 +0200

    Split completed, coming, and todays classes into separate pages, pagination on completed classes

--- backend/dataview/views.py
+++ backend/dataview/views.py
@@ -3,15 +3,33 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from classes.models import Class
 from bookings.models import Booking
+from datetime import date
 # Create your views here.
 
+# Fix this, filter by if date is leq or geq than todays date
 
 
-class ClassListView(ListView):
-    model = Class
-    template_name = 'dataview/list.html'
+class CompletedClassesListView(ListView):
+    queryset = Class.objects.filter(date__lt=date.today()).reverse()
+    template_name = 'dataview/completed.html'
     context_object_name = 'classes'
     ordering = ['date']
+    #adds a paginator and page_obj to the context
+    paginate_by = 20
+
+
+class ComingClassesListView(ListView):
+    queryset = Class.objects.filter(date__gt=date.today())
+    template_name = 'dataview/coming.html'
+    context_object_name = 'classes'
+    ordering = ['date']
+
+
+class TodaysClassesListView(ListView):
+     queryset = Class.objects.filter(date = date.today())
+     template_name = 'dataview/home.html'
+     context_object_name = 'classes'
+     ordering = ['date']
 
 # try defining a separate dictionary for the detail view since it needs two models
 # https://stackoverflow.com/questions/12187751/django-pass-multiple-models-to-one-template
