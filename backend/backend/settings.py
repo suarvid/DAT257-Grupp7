1e3fc150be21f2276fade2fdfcd0fa76fcbdd924
Arvid Sundbom <suarvid@student.chalmers.se>
2020-05-14 19:55:34 +0200

    rollback fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=3_$haupanvj5$x@$b%in1&7m9q$xuh(=crapcdxzjb*m362n4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,9 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # has to be changed for Heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = ''
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = ( os.path.join('static'), )
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
