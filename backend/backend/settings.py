e4f9424ee56b7c73885f13501059520cd7c4188c
Arvid Sundbom <suarvid@student.chalmers.se>
2020-05-18 15:51:42 +0200

    added functionality and views for canceling bookings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,13 +128,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-# has to be changed for Heroku
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-# STATIC_ROOT = ''
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = ( os.path.join('static'), )
 STATIC_URL = '/static/'
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.utils import timezone
 from classes.models import Class
 # Create your models here.
+
+
 class Booking(models.Model):
-    name = models.CharField(max_length = 120)
-    email = models.CharField(max_length = 120)
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
     phone_number = models.IntegerField(default=0)
     classID = models.ForeignKey(Class, on_delete=models.CASCADE)
 
@@ -17,5 +19,15 @@ class Booking(models.Model):
         c.save()
         super().save(*args, **kwargs)
 
+    def delete(self, *args, **kwargs):
+        c = Class.objects.get(id=self.classID.id)
+        c.registered_attendees -= 1
+        c.save()
+        super().delete(*args, **kwargs)
+
+    def get_class(self):
+        return Class.objects.get(id=self.classID.id)
+
+        
     class Meta:
         unique_together = [["classID", "phone_number"]]
