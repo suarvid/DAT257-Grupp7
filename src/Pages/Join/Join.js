961f8cb4a80bbe8ebcd9a9fc63b8704935f326ba
Emelie <emelundq@student.chalmers.se>
2020-05-19 13:39:37 +0200

    fix join-layout

--- src/Pages/Join/Join.js
+++ src/Pages/Join/Join.js
@@ -3,17 +3,17 @@ import './Join.css'
 
 function Join() {
     return (
-            <div className="join-container">
-                <div className="info-text">
-                <h3>Vi på Hela Åsa letar ständigt efter nya partners i vårt hälso-kollektiv. Är du intresserad av att bli del i Hela Åsa?</h3> 
-                <p> Kontakta oss gärna via mail så kan vi diskutera vårt framtida partnerskap!</p>
-                <p> Med Vänliga Hälsningar <br></br>
-                    Hela Åsa <br></br>
+            <div className="joinContainer">
+                <div className="joinContent">
+                <h3 className="joinTitle">Vi på Hela Åsa letar ständigt efter nya partners i vårt hälso-kollektiv. Är du intresserad av att bli del i Hela Åsa?</h3> 
+                <p style={{marginBottom:"10px"}}> Kontakta oss gärna via mail så kan vi diskutera vårt framtida partnerskap!</p>
+                <p><strong> Med Vänliga Hälsningar </strong><br/>
+                    Hela Åsa <br/>
                     info@helaasa.se </p>
                 </div>
                 
                 <div className="companies">
-                    <h3>Ingående företag</h3>
+                    <h3 className="joinTitle">Ingående företag:</h3>
                     <a href="https://www.facebook.com/Yogamidus-1001620300228906/"> Yogamidus</a>
                     <a href="http://niamotion.se/">Niamotion</a>
                     <a href="http://www.unikarum.se/">UnikaRum</a>
