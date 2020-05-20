9901e7f4aaf5139a4366a9636625ed537821acaf
Joel Rudsberg <rudsberg@live.se>
2020-05-20 09:39:36 +0200

    Improved styling to logo in header component and refactored indentation

--- src/Components/Banner/BannerComponent.js
+++ src/Components/Banner/BannerComponent.js
@@ -1,14 +1,13 @@
 import React from "react";
-import niamotion from "./niamotion.jpg";
 import logo from "../Logo2.png";
-import './BannerComponent.css'; 
+import "./BannerComponent.css";
 
-function BannerComponent(){
-    return(
+function BannerComponent() {
+  return (
     <div className="bannerHeader">
-      <img src={logo} alt="logo" height="100%" width="100%"/>
+      <img className="logoHeader" src={logo} alt="logo" />
     </div>
-    )
+  );
 }
 
 export default BannerComponent;
