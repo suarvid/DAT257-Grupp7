5f62e886bc1e2941273b6917fcf2660b2407f650
Joel Rudsberg <rudsberg@live.se>
2020-05-20 09:57:56 +0200

    Imported axios to PostListView and created skelleton for fetchAllPosts function

--- src/Pages/Home/PostListView.js
+++ src/Pages/Home/PostListView.js
@@ -3,6 +3,8 @@ import NewsItem from "../../Components/NewsItem/NewsItem";
 import styles from "./PostListView.css";
 import InfiniteScroll from "react-infinite-scroll-component";
 import "../../globalstyles.css";
+import axios from "axios";
+
 const lorem =
   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet." +
   "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,";
@@ -103,6 +105,16 @@ class PostList extends React.Component {
 
   componentDidMount() {
     this.loadItems();
+  }
+
+  fetchAllPosts = () => {
+    /*
+    axios.get(`http://127.0.0.1:8000/api/instructors/instructors/${this.props.data.instructor}`)
+      .then(response => {
+        this.setState({
+          instructor: response.data,
+        })
+      }).catch(error => { console.log(error) })*/
   }
 
   //Arbitrarily set to loading 3 posts at a time
