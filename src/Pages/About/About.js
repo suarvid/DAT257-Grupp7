6c02c8c4a01dfa088466eb28771f69b993334224
ErikaNystrom <nyerika@student.chalmers.se>
2020-05-20 20:50:08 +0200

    added info from uppdragsgivarna

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -10,7 +10,7 @@ function About() {
         <div> 
             <div className="textPageContainer">
                 <h2>Om Hela Åsa</h2>
-                <img  src={aboutimg} alt="aboutimg" />
+                <img  src={aboutimg} alt="aboutimg" style= {{padding:"5px 20px"}}/>
                 <h3>Företagsnätverket</h3>
                  <p>Hela Åsa startades av Anna Nyström och Lotten Jersby 2019. 
                     Vi är ett företagsnätverk som tror på kraften i att ”tillsammans är vi starka”. 
@@ -26,8 +26,11 @@ function About() {
                       </div>
                     <div className="cardInfo">
                         <h3 className= "infoHeading">Yogamidus</h3>
-                        <p style={{paddingBottom:"5px", width :"100%"}}>Info om Yogamidus....</p>
+                        
                         <a href="https://www.facebook.com/pages/category/Health-Beauty/Yogamidus-1001620300228906/">Vidare till Yogamidus</a>
+                    </div>
+                    <div className="companyInfo">
+                        <p style={{padding:"0px 15px 5px 15px"}}>Yoga erbjuder många verktyg för att hitta in i dig själv och Yogamidus vill vara ditt stöd på vägen. Genom ergonomiska rörelser och positioner lär du känna din kropp och ditt sinne, för att sedan kunna gå vidare in i dig själv om du så vill.</p>
                     </div>
                 </div>
 
@@ -37,8 +40,13 @@ function About() {
                     </div>
                     <div className="cardInfo">
                         <h3 className= "infoHeading">NiaMotion</h3>
-                        <p style={{paddingBottom:"5px"}}>Info om Nia....</p>
                         <a href="http://niamotion.se/">Vidare till NiaMotion</a>
+                    </div>
+                    <div className="companyInfo">
+                        <p style={{padding:"0px 15px 5px 15px"}}>NiaMotion är ett företag som drivs av lust och glädje att dela med sig av rörelse och hälsa
+                        (Nia, Yinyoga mm) framförallt i gemenskap med andra. NIA bidrar bl.a. till medvetenheten
+                        om hur vi genom att använda helheten kropp- själ- tanke- ande i rörelse, kan påverka vårt
+                        välbefinnande.</p>
                     </div>
                 </div>
 
@@ -48,8 +56,15 @@ function About() {
                     </div> 
                     <div className="cardInfo">
                         <h3 className= "infoHeading">UnikaRum</h3>
-                        <p style={{paddingBottom:"5px"}}>Info om UnikaRum....</p>
                         <a href="http://www.unikarum.se/">Vidare till UnikaRum</a>
+                    </div>
+                    <div className="companyInfo">
+                        <p style={{padding:"0px 15px 5px 15px"}}> Företaget UnikaRum har som mål att verka för att människor hittar och tar till vara sina unika
+                        inre och yttre rum. Detta gör vi för att bidra till människors ökade välmående och hälsa.
+                        Vi prövar på olika kreativa uttryck såsom måleri, skrivande, inre bildskapande, dans,
+                        berättande, mm. för att utvecklas och växa som människor.
+                        Namnet och företaget UnikaRum sammanfattar vårt förhållningssätt till människan, att vi alla
+                        är unika och har våra individuella unika rum där vi kan finna de skatter vi har inom oss.</p>
                     </div>
                 </div>
          </div>
