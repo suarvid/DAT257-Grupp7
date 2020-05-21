5f8aec1bd952cd9d3ac7443bbb342b35b4884d14
Joel Rudsberg <rudsberg@live.se>
2020-05-21 17:29:13 +0200

    Improved styling

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import Footer from "./Components/FooterComponent/FooterComponent";
 import Banner from "./Components/Banner/BannerComponent";
 
 export default function App() {
-  const footerHeight = '11rem'
   return (
     <div style={{ minHeight: "100vh", position: "relative" }}>
-      <div style={{ paddingBottom: footerHeight }}>
+      <div style={{ paddingBottom: '11.8rem' }}>
         <Banner />
         <HamburgerMenu />
         <BaseRouter></BaseRouter>
       </div>
-      <Footer style={{ height: footerHeight }} />
+      <Footer />
     </div>
   );
 }
