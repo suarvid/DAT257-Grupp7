bd32eeecebe0240ce0049426a7b763dc6c419dfb
Joel Rudsberg <rudsberg@live.se>
2020-05-21 17:25:04 +0200

    Improved styling booking items

--- src/Pages/Booking/BookingItem.js
+++ src/Pages/Booking/BookingItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../../src/globalstyles.css";
 import "../Booking/Booking.css";
+import CardWrapper from "../../CardWrapper/CardWrapper";
 
 export default function BookingItem(props) {
   const {
@@ -15,30 +16,42 @@ export default function BookingItem(props) {
   } = props;
 
   return (
-    <div className="bookingcomponent">
-      <div>
-        <div className="content">
-          <h3 style={{ fontSize: 16, marginLeft: 0, paddingLeft: 0, marginBottom: 10 }}>{`${activityName} ${date} ${time}`}</h3>
+    <CardWrapper>
+      <div className="bookingcomponent" style={{ position: 'relative' }}>
+        <div>
+          <div className="content" style={{marginBottom: 14}}>
+            <h3
+              style={{
+                fontSize: 18,
+                marginLeft: 0,
+                paddingLeft: 0,
+                marginBottom: '12 !important',
+              }}
+            >{`${activityName}`}</h3>
+          </div>
+          <div className="content">
+            <p>{`${"Tid:"} ${date} ${time}`}</p>
+          </div>
+          <div className="content">
+            <p>{`${"Instruktör:"} ${instructorName}`}</p>
+          </div>
+          <div className="content">
+            <p>{`${"Plats:"} ${locationName}`}</p>
+          </div>
+          <div className="content">
+            <p>{`${"Lediga platser"} ${remainingSpots}`}</p>
+          </div>
         </div>
-        <div className="content">
-          <p>{`${"Instruktör:"} ${instructorName}`}</p>
-        </div>
-        <div className="content">
-          <p>{`${"Plats:"} ${locationName}`}</p>
-        </div>
-        <div className="content">
-          <p>{`${"Lediga platser"} ${remainingSpots}`}</p>
+        <div style={{ position: 'absolute', right: 20, bottom: 20 }}>
+          <button
+            className="primary_button_large"
+            onClick={onClick}
+            disabled={!isBookable}
+          >
+            {isBookable ? "Boka" : "Fullt"}
+          </button>
         </div>
       </div>
-      <div style={{ float: "right", marginRight: 0, marginBottom: 40 }}>
-        <button
-          className="primary_button_large"
-          onClick={onClick}
-          disabled={!isBookable}
-        >
-          {isBookable ? "Boka" : "Fullt"}
-        </button>
-      </div>
-    </div>
+    </CardWrapper>
   );
 }
